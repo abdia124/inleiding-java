4953455c16babc7c87dc76f7b7a38bfc5b71c84f
abdia124 <aabdulqadir@roc-dev.com>
2020-09-15 09:45:58 +0200

    Change text from "hallo klas 8A!" to "hallo studenten"

---
+++ src/h01/hoofdstuk2.java
@@ -0,0 +1,17 @@
+package h01;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class hoofdstuk2 extends Applet {
+
+
+    public void init() { setBackground( new Color(255, 248, 220));
+
+    }
+
+
+    public void paint(Graphics g) {
+g.drawString("Welcome to Java!!", 50, 60);
+    }
+}
