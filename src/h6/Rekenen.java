a755f3d0067d86f7d26ecf8d6341d81e4c8dbd89
abdia124 <aabdulqadir@roc-dev.com>
2020-09-25 13:05:12 +0200

    6.1 gemaakt; niks aangepast

---
+++ src/h6/Rekenen.java
@@ -0,0 +1,27 @@
+package h6;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Rekenen extends Applet {
+    int a, b;
+    double uitkomst;
+
+    public void init() {
+        setBackground(Color.white);
+        setSize(1000, 1000);
+        a = 113;
+        b = 4;
+        uitkomst = a / b;
+    }
+
+    public void paint(Graphics g) {
+        g.drawString("de uitkomst is " + uitkomst, 150, 50);
+        g.drawString("Jan krijgt" + uitkomst, 150, 70);
+        g.drawString("Ali krijgt" + uitkomst, 150, 90);
+        g.drawString("Jeannette krijgt" + uitkomst, 150, 110);
+        g.drawString("Abdi krijgt" + uitkomst, 150, 130);
+    }
+
+
+}
