c0dbbc034b1df3a2a5c5545688a6b114971b85ae
abdia124 <aabdulqadir@roc-dev.com>
2020-10-02 13:17:29 +0200

    8.2 begin

---
+++ src/h8/OpenAvonden.java
@@ -0,0 +1,26 @@
+package h8;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class OpenAvonden extends Applet {
+Button knop;
+Button knop2;
+Button knop3;
+Button knop4;
+
+
+    public void init() {
+        setBackground(Color.white);
+        setSize(1000,1000);
+        knop=new Button();
+        knop.setLabel("");
+
+    }
+    public void paint(Graphics g){
+
+
+
+
+    }
+}
