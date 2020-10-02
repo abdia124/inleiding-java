6c19a3758abc5e4b762c7f0194c141c4de0148ad
abdia124 <aabdulqadir@roc-dev.com>
2020-10-02 12:23:32 +0200

    8.1 Afgerond

--- src/h8/Tekstvakken.java
+++ src/h8/Tekstvakken.java
@@ -4,55 +4,60 @@ import java.applet.Applet;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Test extends Applet {
+public class Tekstvakken extends Applet {
 
     Button knop;
     Button klikie;
     TextField Tekstvak;
     Color backgroundColor = Color.white;
+    String message;
 
     public void init() {
         setSize(1000, 1000);
-        Tekstvak = new TextField("hey hoi",30);
+        Tekstvak = new TextField("hey hoi", 30);
         add(Tekstvak);
         Tekstvak.addActionListener(new Kladje());
 
+        message = "Hallo";
         knop = new Button();
         knop.setLabel("Niet klikken dankje.");
         add(knop);
-        knop.addActionListener(new Tekstklad());
+        knop.addActionListener(new OkListener());
 
         klikie = new Button();
         klikie.setLocation(150, 150);
         klikie.setLabel("Reset");
         add(klikie);
-        klikie.addActionListener(new Kladtekst());
+        klikie.addActionListener(new ResetListener());
 
     }
 
 
     public void paint(Graphics g) {
-
+        g.drawString(message, 50, 100);
     }
 
-    class Tekstklad implements ActionListener {
+    class OkListener implements ActionListener {
 
         public void actionPerformed(ActionEvent e) {
             System.out.println("Knopklik");
             setBackground(Color.pink);
+            message=Tekstvak.getText();
             repaint();
         }
     }
-    class Kladtekst implements ActionListener{
+
+    class ResetListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             System.out.println("klik");
             setBackground(backgroundColor);
-
+            Tekstvak.setText("");
             repaint();
 
         }
     }
-    class Kladje implements ActionListener{
+
+    class Kladje implements ActionListener {
 
         public void actionPerformed(ActionEvent e) {
             repaint();
