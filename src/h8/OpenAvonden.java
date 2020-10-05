f8cfefa697eebef3fa3ed6a29e8141d4ce7b748a
abdia124 <aabdulqadir@roc-dev.com>
2020-10-05 15:06:36 +0200

    meer variabelen toegevoegd

--- src/h8/OpenAvonden.java
+++ src/h8/OpenAvonden.java
@@ -6,42 +6,49 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class OpenAvonden extends Applet {
-Button knop;
-Button knop2;
-Button knop3;
-Button knop4;
+    Button knop;
+    Button knop2;
+    Button knop3;
+    Button knop4;
+    int mannen;
+    int vrouwen;
+    int jongens;
+    int meisjes;
 
 
     public void init() {
         setBackground(Color.white);
-        setSize(1000,1000);
-        knop=new Button();
-        knop.setLabel("");
-        knop2=new Button();
-        knop2.setLabel("");
-        knop3=new Button();
-        knop3.setLabel("");
-        knop4=new Button();
-        knop4.setLabel("");
-        add (knop);
+        setSize(1000, 1000);
+        knop = new Button();
+        knop.setLabel("Mannen");
+        knop2 = new Button();
+        knop2.setLabel("Vrouwen");
+        knop3 = new Button();
+        knop3.setLabel("Jongens");
+        knop4 = new Button();
+        knop4.setLabel("Meisjes");
+        add(knop);
         add(knop2);
         add(knop3);
         add(knop4);
-
-    }
-    public void paint(Graphics g){
-
-
+        knop.addActionListener(new Avond());
 
 
     }
-    class Avond implements ActionListener{
+
+    public void paint(Graphics g) {
+g.drawString("Het aantal mannen zijn"+ mannen,150,150);
+g.drawString("Het aantal vrouwen zijn"+ 0,150,170);
+g.drawString("Het aantal jongens zijn"+ 0,150,190);
+g.drawString("Het aantal meisjes zijn"+ 0,150,210);
+    }
+
+    class Avond implements ActionListener {
+
 
         public void actionPerformed(ActionEvent e) {
-
         }
     }
-
 
 
 }
