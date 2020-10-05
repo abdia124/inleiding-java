4eb4d2154bd3410f54b0848a5f519c0e017c6867
abdia124 <aabdulqadir@roc-dev.com>
2020-10-05 10:51:12 +0200

    variabelen toegevoegd aan 8.2

--- src/h8/OpenAvonden.java
+++ src/h8/OpenAvonden.java
@@ -2,6 +2,8 @@ package h8;
 
 import java.applet.Applet;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class OpenAvonden extends Applet {
 Button knop;
@@ -15,6 +17,16 @@ Button knop4;
         setSize(1000,1000);
         knop=new Button();
         knop.setLabel("");
+        knop2=new Button();
+        knop2.setLabel("");
+        knop3=new Button();
+        knop3.setLabel("");
+        knop4=new Button();
+        knop4.setLabel("");
+        add (knop);
+        add(knop2);
+        add(knop3);
+        add(knop4);
 
     }
     public void paint(Graphics g){
@@ -23,4 +35,13 @@ Button knop4;
 
 
     }
+    class Avond implements ActionListener{
+
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+
+
 }
