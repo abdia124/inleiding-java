1bfa9e8c93e51e30ecf608f3cf0f83520315b6e7
abdia124 <aabdulqadir@roc-dev.com>
2020-10-08 14:38:51 +0200

    begin gemaakt aan 8.3

---
+++ src/h8/Berekeningen.java
@@ -0,0 +1,34 @@
+package h8;
+
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Berekeningen extends Applet {
+    Button knop;
+    TextField tekstvak;
+
+
+    public void init() {
+        setBackground(Color.white);
+        setSize(1000, 1000);
+        tekstvak = new TextField("nee", 50);
+        add(knop);
+        add(tekstvak);
+        tekstvak.addActionListener(new Btw());
+
+    }
+
+
+    public void paint(Graphics g) {
+
+    }
+
+    class Btw implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+}
---
+++ src/h8/Test.java
@@ -0,0 +1,43 @@
+package h8;
+
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Test extends Applet {
+    Button knop;
+    TextField tekstvak;
+
+    public void init() {
+        knop = new Button();
+        setBackground(Color.white);
+        setSize(1000, 1000);
+        tekstvak = new TextField("nee", 50);
+        add(knop);
+        add(tekstvak);
+        tekstvak.addActionListener(new Btw());
+        knop.addActionListener(new Waarde());
+
+    }
+
+
+    public void paint(Graphics g) {
+
+    }
+
+    class Btw implements ActionListener {
+
+
+
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+    class Waarde implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+}
