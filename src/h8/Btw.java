093b99c50bcd055faeef371cf3d6855ad8f34b14
abdia124 <aabdulqadir@roc-dev.com>
2020-10-19 13:44:45 +0200

    8.3 Afgerond

--- src/h8/Btw.java
+++ src/h8/Btw.java
@@ -5,39 +5,50 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Test extends Applet {
+public class Btw extends Applet {
+
     Button knop;
     TextField tekstvak;
+    Double getal;
 
     public void init() {
+        System.out.println();
         knop = new Button("Ok");
         setBackground(Color.white);
         setSize(1000, 1000);
         tekstvak = new TextField("", 50);
         add(knop);
         add(tekstvak);
-        tekstvak.addActionListener(new Btw());
+        tekstvak.addActionListener(new Waarde());
         knop.addActionListener(new Waarde());
+        getal = 0.0;
 
     }
 
 
     public void paint(Graphics g) {
-
+        g.drawString("De btw is" + getal, 100, 100);
     }
 
-    class Btw implements ActionListener {
-
+    class Betaling implements ActionListener {
 
 
         public void actionPerformed(ActionEvent e) {
 
         }
     }
+
     class Waarde implements ActionListener {
 
         public void actionPerformed(ActionEvent e) {
+            String imput = tekstvak.getText();
+            System.out.println(imput);
+            getal = Double.parseDouble(imput);
+            getal *= 1.21;
+            System.out.println(getal);
 
+
+            repaint();
         }
     }
 }
