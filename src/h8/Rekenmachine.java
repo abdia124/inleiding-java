7329210f00cad3f91539bfe7f38213b7c30030df
abdia124 <aabdulqadir@roc-dev.com>
2020-10-20 10:20:01 +0200

    begin gemaakt bij praktijkopdracht

---
+++ src/h8/Rekenmachine.java
@@ -0,0 +1,74 @@
+package h8;
+
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Rekenmachine extends Applet {
+    Button knop;
+    Button knop2;
+    Button knop3;
+    Button knop4;
+    TextField tekstvak;
+    TextField tekstvak2;
+    Double getal;
+
+
+    public void init() {
+        setBackground(Color.white);
+        setSize(1000,1000);
+        knop=new Button("+");
+        knop2=new Button("-");
+        knop3=new Button("*");
+        knop4=new Button("/");
+        tekstvak=new TextField();
+        tekstvak2=new TextField();
+        add(knop);
+        add(knop2);
+        add(knop3);
+        add(knop4);
+        add(tekstvak);
+        add(tekstvak2);
+        tekstvak.addActionListener(new plus());
+        tekstvak2.addActionListener(new plus());
+        tekstvak.addActionListener(new min());
+        tekstvak2.addActionListener(new min());
+        tekstvak.addActionListener(new keer());
+        tekstvak2.addActionListener(new keer());
+        tekstvak.addActionListener(new gedeeld());
+        tekstvak2.addActionListener(new gedeeld());
+        getal=0.0;
+    }
+
+
+    public void paint(Graphics g) {
+
+    }
+    class plus implements ActionListener{
+
+        public void actionPerformed(ActionEvent e) {
+        String imput=tekstvak.getText();
+        getal= Double.parseDouble(imput);
+        System.out.println(imput);
+        getal+=0;
+        }
+    }
+    class min implements ActionListener{
+
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+    class keer implements ActionListener{
+
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+    class gedeeld implements ActionListener{
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+}
