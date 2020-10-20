b7570a1d62a350d5f771a5c56aa4600f2c863fa0
abdia124 <aabdulqadir@roc-dev.com>
2020-10-20 12:36:51 +0200

    variabelen toegevoegd aan praktijkopdracht

--- src/h8/Rekenmachine.java
+++ src/h8/Rekenmachine.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Rekenmachine extends Applet {
+
     Button knop;
     Button knop2;
     Button knop3;
@@ -13,17 +14,20 @@ public class Rekenmachine extends Applet {
     TextField tekstvak;
     TextField tekstvak2;
     Double getal;
+    int uitkomst;
+    int a;
+    int b;
 
 
     public void init() {
         setBackground(Color.white);
-        setSize(1000,1000);
-        knop=new Button("+");
-        knop2=new Button("-");
-        knop3=new Button("*");
-        knop4=new Button("/");
-        tekstvak=new TextField();
-        tekstvak2=new TextField();
+        setSize(1000, 1000);
+        knop = new Button("+");
+        knop2 = new Button("-");
+        knop3 = new Button("*");
+        knop4 = new Button("/");
+        tekstvak = new TextField();
+        tekstvak2 = new TextField();
         add(knop);
         add(knop2);
         add(knop3);
@@ -38,35 +42,43 @@ public class Rekenmachine extends Applet {
         tekstvak2.addActionListener(new keer());
         tekstvak.addActionListener(new gedeeld());
         tekstvak2.addActionListener(new gedeeld());
-        getal=0.0;
+        getal = 0.0;
+        a=tekstvak.getText(plus);
+        b=tekstvak2.getText(plus);
+
     }
 
 
     public void paint(Graphics g) {
 
     }
-    class plus implements ActionListener{
+
+    class plus implements ActionListener {
 
         public void actionPerformed(ActionEvent e) {
-        String imput=tekstvak.getText();
-        getal= Double.parseDouble(imput);
-        System.out.println(imput);
-        getal+=0;
+            String imput = tekstvak.getText();
+            getal = Double.parseDouble(imput);
+            System.out.println(imput);
+            getal += 0;
+            repaint();
         }
     }
-    class min implements ActionListener{
 
-        public void actionPerformed(ActionEvent e) {
-
-        }
-    }
-    class keer implements ActionListener{
+    class min implements ActionListener {
 
         public void actionPerformed(ActionEvent e) {
 
         }
     }
-    class gedeeld implements ActionListener{
+
+    class keer implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    class gedeeld implements ActionListener {
         public void actionPerformed(ActionEvent e) {
 
         }
