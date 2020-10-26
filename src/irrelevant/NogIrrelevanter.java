5bf1d1cda40f79936dde5f4607ed2ded392f2d43
abdia124 <aabdulqadir@roc-dev.com>
2020-10-26 10:24:55 +0100

    hoofdstuk 10 te oefenen

--- src/irrelevant/NogIrrelevanter.java
+++ src/irrelevant/NogIrrelevanter.java
@@ -2,17 +2,45 @@ package irrelevant;
 
 import java.applet.Applet;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class NogIrrelevanter extends Applet {
+    int leeftijd;
+    TextField tekstvak;
+    Label label;
+    String tekst;
 
     public void init() {
         setBackground(Color.white);
-        setSize(1000,1000);
-    }
+        setSize(1000, 1000);
+        tekstvak = new TextField("", 5);
+        tekstvak.addActionListener(new VakListener());
+        tekst = "";
 
+        label = new Label("Geef uw leeftijd in en druk op enter");
+
+        add(label);
+        add(tekstvak);
+
+
+    }
 
 
     public void paint(Graphics g) {
 
+    }
+
+    class VakListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            String s;
+
+            s = tekstvak.getText();
+            leeftijd = Integer.parseInt(s);
+            if (leeftijd > 20) {
+                tekst = "U bent meerderjarig.";
+                repaint();
+            }
+        }
     }
 }
