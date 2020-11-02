1676c143854d0c40b4cbbe5d59dab45df5bac0a1
abdia124 <aabdulqadir@roc-dev.com>
2020-11-02 13:18:35 +0100

    10.5 compleet

--- src/h10/Getallen.java
+++ src/h10/Getallen.java
@@ -8,26 +8,30 @@ import java.awt.event.ActionListener;
 public class Getallen extends Applet {
     Button knop;
     TextField tekstvak;
-    int cijfer;
-    String a;
-    int gemiddelde;
+    Double cijfer;
+    String a, tekst, tekst2;
+    Double uitkomst;
     Label label;
 
 
     public void init() {
-        label= new Label("voer cijfer in");
+        label = new Label("voer cijfer in");
+        add(label);
         tekstvak = new TextField();
         knop = new Button("OK");
         add(tekstvak);
         add(knop);
         tekstvak.addActionListener(new Ongeveer());
-
+        tekstvak.addActionListener(new Geslaagd());
+        tekst = "";
+        tekst2 = "";
 
     }
 
 
     public void paint(Graphics g) {
-        g.drawString("Het gemiddelde is ", 50, 50);
+        g.drawString("" + tekst, 50, 50);
+        g.drawString("" + tekst2, 50, 100);
     }
 
 
@@ -35,17 +39,39 @@ public class Getallen extends Applet {
         public void actionPerformed(ActionEvent e) {
 
             a = tekstvak.getText();
-            cijfer = Integer.parseInt(a);
-
-
-
-
-
-
-
+            cijfer = Double.parseDouble(a);
+            if (cijfer > 5.45) {
+                tekst = "je hebt een voldoende";
+            } else {
+                tekst = "je hebt een onvoldoende";
             }
+            if (cijfer > 10) {
+                tekst = "je cijfer kan niet hoger zijn dan 10";
+            }
+
+            repaint();
+
+
         }
-
-
     }
+
+    class Geslaagd implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            a = tekstvak.getText();
+            cijfer = Double.parseDouble(a);
+            if (cijfer > 5.45) {
+                tekst2 = "je bent geslaagd";
+            } else {
+                tekst2 = "je bent gezakt";
+            }
+            if (cijfer > 10) {
+                tekst2 = "";
+            }
+
+            repaint();
+
+
+        }
+    }
+
 }
