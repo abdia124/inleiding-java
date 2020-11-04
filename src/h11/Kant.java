d6d5b72db131025c604d87acc0f3567634f876ec
abdia124 <aabdulqadir@roc-dev.com>
2020-11-04 12:29:02 +0100

    11.5 compleet

---
+++ src/h11/Kant.java
@@ -0,0 +1,21 @@
+package h11;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Kant extends Applet {
+
+    public void init() {
+
+    }
+
+
+    public void paint(Graphics g) {
+        g.drawRect(100,100,10,10);
+        g.drawRect(110,110,10,10);
+        g.drawRect(120,120,10,10);
+        g.drawRect(130,130,10,10);
+        g.drawRect(140,140,10,10);
+
+    }
+}
