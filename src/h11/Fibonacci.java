551b300d173b0852e92f763df53ef2cbe1d656d3
abdia124 <aabdulqadir@roc-dev.com>
2020-11-04 09:43:15 +0100

    11.3 compleet

--- src/h11/Fibonacci.java
+++ src/h11/Fibonacci.java
@@ -11,9 +11,21 @@ public class Fibonacci extends Applet {
 
 
     public void paint(Graphics g) {
-int x=0;
+        int x = 0;
+        int antwoord=0;
+        int antwoord2=1;
+        int uitkomst= antwoord+antwoord2;
 
 
+        for (int teller = 0; teller < 40; teller++) {
+
+            x+=20;
+            g.drawString(""+uitkomst,100,x);
+
+            antwoord=antwoord2;
+            antwoord2=uitkomst;
+            uitkomst=antwoord+antwoord2;
+        }
 
 
     }
