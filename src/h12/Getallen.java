0fe7d6d555e4cb6ab84c8f41f4eae5dfc0a39a9f
abdia124 <aabdulqadir@roc-dev.com>
2020-11-11 12:37:58 +0100

    12.1 begin

---
+++ src/h12/Getallen.java
@@ -0,0 +1,19 @@
+package h12;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Getallen extends Applet {
+    double gemiddelde;
+    int beginWaarde;
+    int eindWaarde;
+
+    public void init() {
+
+    }
+
+
+    public void paint(Graphics g) {
+
+    }
+}
