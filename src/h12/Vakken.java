b79fed2ee2672663ae7c6948aef515353cc56861
abdia124 <aabdulqadir@roc-dev.com>
2020-11-12 12:25:33 +0100

    12.3 begin

---
+++ src/h12/Vakken.java
@@ -0,0 +1,17 @@
+package h12;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Vakken extends Applet {
+    TextField[] tekstvak = new TextField[5];
+
+    public void init() {
+
+    }
+
+
+    public void paint(Graphics g) {
+
+    }
+}
