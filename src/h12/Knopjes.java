e59700ded70e3c5cedbaa3ca0474fdf6c872cebe
abdia124 <aabdulqadir@roc-dev.com>
2020-11-12 10:54:17 +0100

    12.2 begin

---
+++ src/h12/Knopjes.java
@@ -0,0 +1,21 @@
+package h12;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Knopjes extends Applet {
+    Button[] knoppen =new Button[25];
+
+    public void init() {
+
+    }
+
+    public void paint(Graphics g) {
+
+        for (int teller = 0; teller < knoppen.length; teller++) {
+            knoppen[teller]=new Button(""+teller);
+            add(knoppen[teller]);
+
+        }
+    }
+}
