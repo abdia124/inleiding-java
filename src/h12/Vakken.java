b0238a06fec0d4b839d353ac0405e775b747c409
abdia124 <aabdulqadir@roc-dev.com>
2020-11-17 09:13:29 +0100

    12.3 begin

--- src/h12/Vakken.java
+++ src/h12/Vakken.java
@@ -2,6 +2,8 @@ package h12;
 
 import java.applet.Applet;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Vakken extends Applet {
     TextField[] tekstvak = new TextField[5];
@@ -12,6 +14,15 @@ public class Vakken extends Applet {
 
 
     public void paint(Graphics g) {
+        for (int teller = 0; teller < tekstvak.length; teller++) {
+            tekstvak[teller] = new TextField("" + teller);
+            add(tekstvak[teller]);
 
+        }
+    }
+    class VakListener implements ActionListener{
+        public void actionPerformed(ActionEvent e) {
+
+        }
     }
 }
