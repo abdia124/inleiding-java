c2d0cae0dff004fe1935f465bc5c0403a263728e
abdia124 <aabdulqadir@roc-dev.com>
2020-11-17 13:56:46 +0100

    12.3 meer variabelen

--- src/h12/Vakken.java
+++ src/h12/Vakken.java
@@ -6,23 +6,31 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Vakken extends Applet {
-    TextField[] tekstvak = new TextField[5];
+    TextField[] tekstvak;
+    Button knop;
+    int teller;
 
     public void init() {
-
-    }
-
-
-    public void paint(Graphics g) {
-        for (int teller = 0; teller < tekstvak.length; teller++) {
+        tekstvak= new TextField[6];
+        knop = new Button();
+        add(knop);
+        for (teller = 0; teller < tekstvak.length; teller++) {
             tekstvak[teller] = new TextField("" + teller);
             add(tekstvak[teller]);
+            tekstvak[teller].addActionListener(new VakListener());
 
         }
+
     }
-    class VakListener implements ActionListener{
-        public void actionPerformed(ActionEvent e) {
+
+    public void paint(Graphics g) {
 
         }
+
+
+    }
+
+class VakListener implements ActionListener {
+    public void actionPerformed(ActionEvent e) {
     }
 }
