d73337c1affc3ce5d151f6b24b348a22556e30c6
abdia124 <aabdulqadir@roc-dev.com>
2020-11-18 13:55:13 +0100

    12.3 variabelen toegevoegd

--- src/h12/Vakken.java
+++ src/h12/Vakken.java
@@ -4,24 +4,24 @@ import java.applet.Applet;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Arrays;
 
 public class Vakken extends Applet {
+    int[] getallen = new int[6];
     TextField[] tekstvakken = new TextField[6];
-    int[] getallen;
     Button knop;
     Label label;
 
 
     public void init() {
 
-        setSize(500,500);
+        setSize(500, 500);
 
         label = new Label("zet 5 getallen in de tekstvakken");
         knop = new Button("volgorde");
-        knop.addActionListener(new VakListener());
         add(knop);
         add(label);
-        for (int teller = 0; teller < 6; teller++) {
+        for (int teller = 0; teller < tekstvakken.length; teller++) {
             tekstvakken[teller] = new TextField("" + teller);
             add(tekstvakken[teller]);
             tekstvakken[teller].addActionListener(new VakListener());
@@ -31,24 +31,30 @@ public class Vakken extends Applet {
     }
 
     public void paint(Graphics g) {
+        knop.addActionListener(new VakListener());
+        int x = 20;
+        for (int i = 0; i < getallen.length; i++) {
+            g.drawString("" + getallen[i], x * i + 50, 50);
+        }
+    }
+
+
+    class VakListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            TextField[] tekstvakken = new TextField[6];
+            int[] getallen = new int[6];
+            String a;
+            for (int cijfer = 0; cijfer < tekstvakken.length; cijfer++) {
+                a = tekstvakken[cijfer].getText();
+                getallen[cijfer] = Integer.parseInt(a);
+
+
+            }
+            Arrays.sort(getallen);
+            repaint();
 
         }
 
-
     }
 
-class VakListener implements ActionListener {
-    public void actionPerformed(ActionEvent e) {
-        String a;
-        for (int cijfer = 0; cijfer <6 ; cijfer++) {
-
-
-
-
-
-        }
-
-
-
-    }
 }
