c603db171e1b385214c7fff2d63c90ad1b3b6082
abdia124 <aabdulqadir@roc-dev.com>
2020-11-19 14:59:35 +0100

    12.3 compleet

--- src/h12/Vakken.java
+++ src/h12/Vakken.java
@@ -7,16 +7,13 @@ import java.awt.event.ActionListener;
 import java.util.Arrays;
 
 public class Vakken extends Applet {
-    int[] getallen = new int[6];
-    TextField[] tekstvakken = new TextField[6];
+    int[] getallen = new int[5];
+    TextField[] tekstvakken = new TextField[5];
     Button knop;
     Label label;
 
-
     public void init() {
-
         setSize(500, 500);
-
         label = new Label("zet 5 getallen in de tekstvakken");
         knop = new Button("volgorde");
         add(knop);
@@ -25,32 +22,24 @@ public class Vakken extends Applet {
             tekstvakken[teller] = new TextField("" + teller);
             add(tekstvakken[teller]);
             tekstvakken[teller].addActionListener(new VakListener());
-
-
-
         }
-
     }
 
     public void paint(Graphics g) {
         knop.addActionListener(new VakListener());
         int x = 20;
-        for (int i = 0; i < getallen.length; i++) {
-            g.drawString("" + getallen[i], x * i + 50, 50);
+        for (int teller2 = 0; teller2 < getallen.length; teller2++) {
+            g.drawString("" + getallen[teller2], x * teller2 + 50, 50);
         }
     }
 
-
     class VakListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            TextField[] tekstvakken = new TextField[6];
-            int[] getallen = new int[6];
             String a;
             for (int cijfer = 0; cijfer < tekstvakken.length; cijfer++) {
                 a = tekstvakken[cijfer].getText();
+                tekstvakken[cijfer].setText("");
                 getallen[cijfer] = Integer.parseInt(a);
-
-
             }
             Arrays.sort(getallen);
             repaint();
