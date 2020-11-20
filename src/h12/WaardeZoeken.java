0ac0089368aff81b53b11ed13ad9e5c0efbc9ba1
abdia124 <aabdulqadir@roc-dev.com>
2020-11-20 11:54:41 +0100

    12.4 variabelen toegevoegd

--- src/h12/WaardeZoeken.java
+++ src/h12/WaardeZoeken.java
@@ -4,8 +4,15 @@ import java.applet.Applet;
 import java.awt.*;
 
 public class WaardeZoeken extends Applet {
-
+TextField tekstvak;
+Button knop;
     public void init() {
+        tekstvak =  new TextField();
+        knop = new Button();
+        add(knop);
+        add(tekstvak);
+        int index;
+        int[] getallen = new int[]{};
         setSize(1000,1000);
     }
 
