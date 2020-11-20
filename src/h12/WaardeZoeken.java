945337ffa4aaa867e6b5763bba6ed21c3ce72b10
abdia124 <aabdulqadir@roc-dev.com>
2020-11-20 10:03:10 +0100

    12.4 begin

---
+++ src/h12/WaardeZoeken.java
@@ -0,0 +1,16 @@
+package h12;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class WaardeZoeken extends Applet {
+
+    public void init() {
+        setSize(1000,1000);
+    }
+
+
+    public void paint(Graphics g) {
+
+    }
+}
