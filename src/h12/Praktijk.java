5586c701e94554c26452541559425ecb38ecfb90
abdia124 <aabdulqadir@roc-dev.com>
2020-11-25 13:43:35 +0100

    Praktijk + variabelen

--- src/h12/Praktijk.java
+++ src/h12/Praktijk.java
@@ -5,51 +5,83 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+//ik weet niet waarom, maar je moet de applet een beetje schuiven voordat de code pas werkt//
+
 public class Praktijk extends Applet {
     TextField naam;
-    TextField tellie;
     Button knop;
+    String a,tekst, tekst2;
+    int telefoon;
     Label label;
-    Label label2;
-    int a = 0;
-    boolean gevonden = false;
-    String[] namen;
-    int[] telefoon;
 
     public void init() {
+        label = new Label("voer een getal van 1 t/m 10 in");
+        add(label);
         knop = new Button("Bevestig");
         naam = new TextField();
-        tellie = new TextField();
-        label = new Label("voer je naam in");
-        label2 = new Label(" voer je telefoonnummer in");
-
-        add(label);
-        add(label2);
+        setSize(1000,1000);
         add(naam);
-        add(tellie);
         add(knop);
-
         naam.addActionListener(new Listener());
-        tellie.addActionListener(new Listener());
         knop.addActionListener(new Listener());
 
-        namen = new String[10];
-        telefoon = new int[10];
     }
-
-
     public void paint(Graphics g) {
-
-        label.setLocation(100, 50);
-        naam.setLocation(200, 50);
-        label2.setLocation(100, 100);
-        tellie.setLocation(270, 100);
-
+        if (tekst == null) tekst = "";
+        if (tekst2 == null)tekst2 = "";
+        g.drawString(tekst,100,100 );
+        g.drawString(tekst2,100,150);
     }
-    class Listener implements ActionListener{
-        public void actionPerformed(ActionEvent e) {
 
+    class Listener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            a = naam.getText();
+            telefoon = Integer.parseInt(a);
+            switch (telefoon) {
+                case 1:
+                    tekst ="naam Abdi" ;
+                    tekst2 = "telefoonummer 0639294929";
+                    break;
+                case 2:
+                    tekst = "naam Rick";
+                    tekst2 = "telefoonummer 0629583958";
+                    break;
+                case 3:
+                    tekst = "naam Foekas";
+                    tekst2 = "telefoonummer 0610297548";
+                    break;
+                case 4:
+                    tekst = "naam Jeff";
+                    tekst2 = "telefoonummer 0638593859";
+                    break;
+                case 5:
+                    tekst = "naam Rico";
+                    tekst2 = "telefoonummer 0693758392";
+                    break;
+                case 6:
+                    tekst = "naam Perry";
+                    tekst2 = "telefoonummer 0649203947";
+                    break;
+                case 7:
+                    tekst = "naam Ferb";
+                    tekst2 = "telefoonummer 0673859385";
+                    break;
+                case 8:
+                    tekst = "naam Roger";
+                    tekst2 = "telefoonummer 0655938583";
+                    break;
+                case 9:
+                    tekst = "naam Ace";
+                    tekst2 = "telefoonummer 0692485927";
+                    break;
+                case 10:
+                    tekst = "naam Monkey";
+                    tekst2 = "telefoonummer 06155873947";
+                    break;
+                default:
+                    tekst = "Dit nummer bestaat niet";
+                    break;
+            }
         }
     }
-
 }
