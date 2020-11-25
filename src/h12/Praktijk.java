cee1e335ba16a5732cbc0bed79378ac6f342044c
abdia124 <aabdulqadir@roc-dev.com>
2020-11-25 09:51:52 +0100

    Praktijk + variabelen

--- src/h12/Praktijk.java
+++ src/h12/Praktijk.java
@@ -7,33 +7,43 @@ import java.awt.event.ActionListener;
 
 public class Praktijk extends Applet {
     TextField naam;
-    TextField telefoon;
+    TextField tellie;
     Button knop;
     Label label;
     Label label2;
+    int a = 0;
+    boolean gevonden = false;
+    String[] namen;
+    int[] telefoon;
 
     public void init() {
         knop = new Button("Bevestig");
         naam = new TextField();
-        telefoon = new TextField();
+        tellie = new TextField();
         label = new Label("voer je naam in");
         label2 = new Label(" voer je telefoonnummer in");
+
         add(label);
         add(label2);
         add(naam);
-        add(telefoon);
+        add(tellie);
         add(knop);
+
         naam.addActionListener(new Listener());
-        telefoon.addActionListener(new Listener());
+        tellie.addActionListener(new Listener());
         knop.addActionListener(new Listener());
+
+        namen = new String[10];
+        telefoon = new int[10];
     }
 
 
     public void paint(Graphics g) {
+
         label.setLocation(100, 50);
         naam.setLocation(200, 50);
         label2.setLocation(100, 100);
-        telefoon.setLocation(270, 100);
+        tellie.setLocation(270, 100);
 
     }
     class Listener implements ActionListener{
@@ -41,4 +51,5 @@ public class Praktijk extends Applet {
 
         }
     }
+
 }
