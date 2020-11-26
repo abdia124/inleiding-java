ffb07e09f89ad27c93ea5011b640a21eef0c8ede
abdia124 <aabdulqadir@roc-dev.com>
2020-11-26 15:13:24 +0100

    13.2 + variabelen

--- src/h13/RodeMuur.java
+++ src/h13/RodeMuur.java
@@ -7,18 +7,30 @@ public class RodeMuur extends Applet {
 
 
     public void paint(Graphics g) {
-BaksteenMuur(g,50,50,200,200);
-
+        setSize(1500,1500);
+        BaksteenMuur(g, 50, 50, 300, 180);
 
 
     }
-    void BaksteenMuur(Graphics g,int x,int y, int breedte, int hoogte){
-    int teller;
-    g.setColor(Color.red);
-    g.fillRect(x,y,breedte,hoogte);
 
+    void BaksteenMuur(Graphics g, int x, int y, int breedte, int hoogte) {
 
+        int y2 = y;
+        g.setColor(Color.red);
+        g.fillRect(x, y, breedte, hoogte);
+        g.setColor(Color.black);
+        int hoogte2 = hoogte / 20;
+        int breedte2 = breedte / 20;
+        for (int teller = 0; teller < hoogte * breedte / hoogte2; teller += breedte / 15) {
+            g.drawRect(x, y, breedte2, hoogte2);
+            y += hoogte2;
+            while (y >= hoogte) {
+                y=y2;
+                x += breedte2;
 
+            }
+
+        }
 
 
     }
