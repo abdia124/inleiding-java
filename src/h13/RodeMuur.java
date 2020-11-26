e151a68d7c26a6eaac773f26471a7ecc66b920cd
abdia124 <aabdulqadir@roc-dev.com>
2020-11-26 12:46:29 +0100

    13.2 begin

---
+++ src/h13/RodeMuur.java
@@ -0,0 +1,20 @@
+package h13;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class RodeMuur extends Applet {
+
+
+    public void paint(Graphics g) {
+BaksteenMuur(g,50,50,200,200);
+    }
+    void BaksteenMuur(Graphics g,int x,int y, int breedte, int hoogte){
+
+
+
+
+
+
+    }
+}
