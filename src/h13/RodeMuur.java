5aca252e9847b6e1382b9ec8b8df9b2de8247907
abdia124 <aabdulqadir@roc-dev.com>
2020-11-30 11:29:37 +0100

    13.2 + 13.3

--- src/h13/RodeMuur.java
+++ src/h13/RodeMuur.java
@@ -6,10 +6,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class RodeMuur extends Applet {
-Button BetonKnop;
-Button SteenKnop;
-
-
+    Button BetonKnop;
+    Button SteenKnop;
+    Color color = Color.gray;
+    boolean draw = false;
 
     public void init() {
         setSize(1500, 1500);
@@ -19,39 +19,18 @@ Button SteenKnop;
         add(SteenKnop);
         BetonKnop.addActionListener(new Betonlistener());
         SteenKnop.addActionListener(new Steenlistener());
+
     }
 
     public void paint(Graphics g) {
-
-        BetonMuur(g, 50, 50, 300, 180);
-        StenenMuur(g, 50, 50, 300, 180);
-    }
-
-    void BetonMuur(Graphics g, int x, int y, int breedte, int hoogte) {
-        int y2 = y;
-        g.setColor(Color.red);
-        g.fillRect(x, y, breedte, hoogte);
-        g.setColor(Color.black);
-        int hoogte2 = hoogte / 10;
-        int breedte2 = breedte / 10;
-        for (int teller = 0; teller < hoogte * breedte / hoogte2; teller += breedte / 15) {
-            g.drawRect(x, y, breedte2, hoogte2);
-            y += hoogte2;
-            while (y >= hoogte) {
-                y = y2;
-                x += breedte2;
-
-            }
-
-        }
-
+        if (draw) drawWall(g, 50, 50, 200, 150, color);
 
     }
 
-    void StenenMuur(Graphics g, int x, int y, int breedte, int hoogte) {
+    void drawWall(Graphics g, int x, int y, int breedte, int hoogte, Color color) {
         int y2 = y;
-        g.setColor(Color.gray);
-        g.fillRect(x, y, breedte, hoogte);
+        g.setColor(color);
+        g.fillRect(x, y, breedte * 22 / 10, hoogte / 10 * 7);
         g.setColor(Color.black);
         int hoogte2 = hoogte / 10;
         int breedte2 = breedte / 10;
@@ -65,14 +44,21 @@ Button SteenKnop;
             }
         }
     }
-    class Betonlistener implements ActionListener{
-        public void actionPerformed(ActionEvent e) {
 
+
+    class Betonlistener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            color = Color.red;
+            draw = true;
+            repaint();
         }
     }
-    class Steenlistener implements ActionListener{
-        public void actionPerformed(ActionEvent e) {
 
+    class Steenlistener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            color = Color.gray;
+            draw = true;
+            repaint();
         }
     }
 }
